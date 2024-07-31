be081f29a6faf0905d7223dbb528b4e78c81a9e4
suratasbasii@gmail.com <suratasbasii@gmail.com>
2024-07-31 10:33:31 +0300

    Add project files.

---
+++ WhileDoWhileKullanimlari/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// Döngüyü önce while ile yazdırıyorum.
+
+int sayac = 0;
+
+Console.WriteLine("Lütfen bir sayı değeri giriniz: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+ while (sayac < limit)
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+}
+
+
+Console.WriteLine("\n----------------------------------------------------------\n");
+
+
+// Döngüyü do while ile yazdırıyorum.
+
+int sayac2 = 0;
+
+Console.WriteLine("Lütfen bir sayı değeri giriniz: ");
+int limit2 = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac2++;
+}
+
+while (sayac2 < limit2);
+
+
+// While döngüsü ile yazdırıldığında kullanıcı eğer negatif değer girerse döngü hiç çalışmaz ama do-while döngüsü ile yazdırılırsa kullanıcı negatif değer girse dahi döngü bir kez çalıştırılır ve Console ekranına metin yazdırılır ancak döngü devam etmez. Yazılan bu metinde döngünün en az bir kez çalıştırılması mantıklı olacağı için do-while ile yazdırmak daha mantıklı olacaktır.
